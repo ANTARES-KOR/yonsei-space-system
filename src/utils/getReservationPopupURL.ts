d716030465cf33249edac322310195162a717f17
antares-kor <tjgurwns5757@gmail.com>
2022-07-24 02:43:26 +0900

    constants 변경

--- src/utils/getReservationPopupURL.ts
+++ src/utils/getReservationPopupURL.ts
@@ -1,3 +1,5 @@
+import { URLs } from "../constants";
+
 interface ReservationPopupURLProps {
   room_uid: string;
   start_time: Date;
@@ -12,7 +14,7 @@ const getReservationPopupURL = ({
   const url_start_time = Math.floor(start_time.getMilliseconds() / 1000);
   const url_end_time = Math.floor(end_time.getMilliseconds() / 1000);
 
-  return `https://space.yonsei.ac.kr/ys_popform.php?mid=K02&start=${url_start_time}&end=${url_end_time}&uid=${room_uid}`;
+  return `${URLs.popupForm}?mid=K02&start=${url_start_time}&end=${url_end_time}&uid=${room_uid}`;
 };
 
 export default getReservationPopupURL;
