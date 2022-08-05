8004ce9cef3aa26f70c783c29058e40ea214597c
eunddodi <87167786+eunddodi@users.noreply.github.com>
2022-08-05 23:17:20 +0900

    [Chore] : Setup/frontend (#19)
    * add gitignore
    
    * build: setup eslint and prettier
    
    Install eslint and prettier packages for typescript and react
    
    Update package.json
    
    * build: setup sass
    
    Install sass with the command below
    - pnpm add sass
    
    Check sass works well by changing css files to scss files
    
    * remove unnecessary default code
    
    Remove default codes generated by Vite
    - App.tsx
    - main.tsx
    
    Remove App.scss
    Remove the contents of index.scss
    
    * build: setup recoil
    
    Install recoil with the command below
    - pnpm add recoil
    
    Put RecoilRoot component to the root component in main.tsx
    
    Co-authored-by: eunddodi <eundppojjack@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import App from './App';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<RecoilRoot>
+			<App />
+		</RecoilRoot>
+	</React.StrictMode>
+);
