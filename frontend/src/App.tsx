8c22a16cfe06865a61dfd6adf818278e00b26fb8
antares-kor <tjgurwns5757@gmail.com>
2022-08-06 16:38:49 +0900

    change test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">연세대학교 공간대관시스템 스크래퍼 입니다!!</div>;
+  return <div className="App">연세대학교 공간대관시스템 스크래퍼입니다!!</div>;
 }
 
 export default App;
