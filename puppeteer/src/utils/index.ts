f6a18fa1d9988a76f8d6f692e4da384a8ddf9246
antares-kor <tjgurwns5757@gmail.com>
2022-08-16 22:07:47 +0900

    delete unnecessary project

--- puppeteer/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as getReservationPopupURL } from "./getReservationPopupURL";
-export { default as loginToYonseiSpaceSystem } from "./loginToYonseiSpaceSystem";
-export { default as windowSet } from "./windowSet";
