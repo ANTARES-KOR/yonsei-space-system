cf6bee77eebf56b07192668954f43f9b6eacab87
eunddodi <eundppojjack@gmail.com>
2022-08-22 21:15:25 +0900

    feat: Implement login feature
    - 로그인 성공 시 메인 페이지로 이동
    - 로그인 실패 시 하단에 노티메시지
    (input에 포커스 잡힐 시 노티메시지 사라짐)
    - 로그인 이뤄지는 동안 로딩화면

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import styles from './LoginPage.module.scss';
 import Button from '../../components/Button';
-
-interface LoginForm {
-  id: number;
-  password: string;
-}
+import Loading from '../../components/Loading';
+import YSSAPi from '../../apis/index';
+import { LoginForm } from '../../interfaces';
 
 const cx = classNames.bind(styles);
 
 function LoginPage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginForm>();
+  const [isLoginFailed, setIsLoginFailed] = useState<boolean | null>(null);
+  const navigate = useNavigate();
+  const { register, handleSubmit, formState } = useForm<LoginForm>();
+  const { errors, isSubmitting } = formState;
 
-  const onSubmit = (data: LoginForm) => {
-    // will be executed when submit event happens
+  const requestLogin = async (data: LoginForm) => {
+    const res = await YSSAPi.login(data);
+    if (res) {
+      navigate('/', { replace: true });
+      return;
+    }
+    setIsLoginFailed(true);
+  };
+
+  const onFocusOnInputBox = () => {
+    setIsLoginFailed(null);
   };
 
   return (
     <div className={cx('container')}>
+      {isSubmitting && <Loading />}
       <h1>Yonsei space system</h1>
       <h5>연세포탈 아이디와 비밀번호를 입력하세요.</h5>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(requestLogin)}>
         <input
           {...register('id', {
             required: '아이디를 입력하세요.',
@@ -36,20 +44,25 @@ function LoginPage() {
             },
           })}
           placeholder="학번 10자리"
+          onFocus={() => onFocusOnInputBox()}
         />
         <div className={cx('error', { hidden: !errors.id })}>
           {errors.id?.message}
         </div>
         <input
-          {...register('password', { required: '비밀번호를 입력하세요' })}
+          {...register('pw', { required: '비밀번호를 입력하세요' })}
           placeholder="비밀번호"
-          type="password"
+          // type="password"
+          onFocus={() => onFocusOnInputBox()}
         />
-        <div className={cx('error', { hidden: !errors.password })}>
-          {errors.password?.message}
+        <div className={cx('error', { hidden: !errors.pw })}>
+          {errors.pw?.message}
         </div>
         <Button label="로그인" fullWidth />
       </form>
+      <div className={cx('error', 'red', { hidden: !isLoginFailed })}>
+        아이디 혹은 비밀번호를 다시 확인해 주세요.
+      </div>
     </div>
   );
 }
