0257440a5b0be5848a98f85725a78756cdb485ae
eunddodi <eundppojjack@gmail.com>
2022-08-26 18:59:39 +0900

    feat: Define reservationStatusState atom obj and interfaces

--- frontend/src/atom.ts
+++ frontend/src/atom.ts
@@ -1,4 +1,58 @@
 import { atom } from 'recoil';
+import { SingleReservationStatus } from './interfaces';
+
+const tmp = [
+  {
+    date: '8/28 (토)',
+    reservations: [
+      {
+        startTime: '06:00',
+        endTime: '08:50',
+        eventName: '[강의(정규)] 학위가운배부',
+      },
+      {
+        startTime: '09:00',
+        endTime: '18:00',
+        eventName: '[강의(정규)] 학위가운배부',
+      },
+      {
+        startTime: '18:10',
+        endTime: '22:00',
+        eventName: '[강의(정규)] 학위가운배부',
+      },
+    ],
+  },
+  {
+    date: '9/3 (토)',
+    reservations: [
+      {
+        startTime: '06:00',
+        endTime: '08:50',
+        eventName: '[강의(정규)] 학위가운배부',
+      },
+      {
+        startTime: '09:00',
+        endTime: '18:00',
+        eventName: '[강의(정규)] 학위가운배부',
+      },
+      {
+        startTime: '18:10',
+        endTime: '22:00',
+        eventName: '[강의(정규)] 학위가운배부',
+      },
+    ],
+  },
+];
+
+const selectState = atom<string | null>({
+  key: 'selectState',
+  default: null,
+});
+
+const reservationStatusState = atom<SingleReservationStatus[]>({
+  key: 'reservationStatusState',
+  default: tmp,
+});
 
 const sessionStorageEffect =
   (key: string) =>
@@ -18,9 +72,4 @@ const isLoginCompletedState = atom({
   effects: [sessionStorageEffect('isLoginCompleted')],
 });
 
-const selectState = atom<string | null>({
-  key: 'selectState',
-  default: null,
-});
-
-export { selectState, isLoginCompletedState };
+export { selectState, reservationStatusState, isLoginCompletedState };
--- frontend/src/interfaces/index.ts
+++ frontend/src/interfaces/index.ts
@@ -2,3 +2,14 @@ export interface LoginForm {
   id: string;
   pw: string;
 }
+
+export interface SingleReservation {
+  startTime: string;
+  endTime: string;
+  eventName: string;
+}
+
+export interface SingleReservationStatus {
+  date: string;
+  reservations: SingleReservation[];
+}
