8f903dd0a5381e8997185a8af59f6029c1d1051b
eunddodi <eundppojjack@gmail.com>
2022-09-04 20:40:05 +0900

    remove: Delete test data

--- frontend/src/atom.ts
+++ frontend/src/atom.ts
@@ -1,64 +1,6 @@
 import { atom } from 'recoil';
 import { SingleReservationStatus } from './interfaces';
 
-const tmp = [
-  {
-    date: '2022-08-28',
-    reservations: [
-      {
-        id: '1',
-        title: '[강의(정규)] 학위가운배부',
-        start: '06:00',
-        end: '08:50',
-        date: '2022-09-03',
-        url: 'https://www.naver.com',
-        allday: false,
-      },
-      {
-        id: '1',
-        title: '[강의(정규)] 학위가운배부',
-        start: '09:00',
-        end: '18:00',
-        date: '2022-09-03',
-        url: 'https://www.naver.com',
-        allday: false,
-      },
-      {
-        id: '1',
-        title: '[강의(정규)] 학위가운배부',
-        start: '18:10',
-        end: '22:00',
-        date: '2022-09-03',
-        url: 'https://www.naver.com',
-        allday: false,
-      },
-    ],
-  },
-  {
-    date: '2022-09-03',
-    reservations: [
-      {
-        id: '1',
-        title: '[강의(정규)] 학위가운배부',
-        start: '06:00',
-        end: '08:50',
-        date: '2022-09-03',
-        url: 'https://www.naver.com',
-        allday: false,
-      },
-      {
-        id: '1',
-        title: '[강의(정규)] 학위가운배부',
-        start: '11:00',
-        end: '18:00',
-        date: '2022-09-03',
-        url: 'https://www.naver.com',
-        allday: false,
-      },
-    ],
-  },
-];
-
 const selectState = atom<string | null>({
   key: 'selectState',
   default: null,
@@ -66,7 +8,7 @@ const selectState = atom<string | null>({
 
 const reservationStatusState = atom<SingleReservationStatus[]>({
   key: 'reservationStatusState',
-  default: tmp,
+  default: [],
 });
 
 const sessionStorageEffect =
